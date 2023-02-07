b0b035ad52519ae7e02d0eccfe94d37378c01073
Boris Glimcher <Boris.Glimcher@emc.com>
2023-02-07 18:02:43 +0200

    Add inventory generated protobufs
    From https://github.com/opiproject/opi-api/blob/main/common/v1/gen/python
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ pydpu/inventory.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2022 Dell Inc, or its subsidiaries.
+
+import grpc
+from .proto.inventory import ipsec_pb2
+from .proto.inventory import ipsec_pb2_grpc
+
+
+def get():
+    print("tbd")
