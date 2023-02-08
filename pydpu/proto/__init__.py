6a82cd03aa5356d14ca79f1e481cc5b3dc8939b3
Boris Glimcher <Boris.Glimcher@emc.com>
2023-02-08 17:48:20 +0200

    Add storage
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pydpu/proto/__init__.py
+++
@@ -1,5 +0,0 @@
-# Add proto dir to PYTHONPATH since protoc does not create relative imports
-# See this for more info https://github.com/protocolbuffers/protobuf/issues/1491
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__)))
---
+++ pydpu/storage.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2022 Dell Inc, or its subsidiaries.
+
+import grpc
+
+from .proto.v1 import frontend_nvme_pcie_pb2
+
+
+def nvme_subsystems():
+    print(
+        "tbd",
+        grpc.StatusCode.UNIMPLEMENTED,
+        frontend_nvme_pcie_pb2.ListNVMeSubsystemsRequest(),
+    )
