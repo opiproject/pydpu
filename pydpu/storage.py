2e7235f4a493cca2d875de79d0d698f1944d0914
Boris Glimcher <Boris.Glimcher@emc.com>
2023-02-14 17:50:41 +0200

    grpc: use with statement
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pydpu/storage.py
+++ pydpu/storage.py
@@ -22,75 +22,75 @@ def list_nvme_subsystems(address):
 
 
 def create_nvme_subsystem(address):
-    channel = grpc.insecure_channel(address)
-    stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
     try:
-        res = stub.CreateNVMeSubsystem(
-            request=frontend_nvme_pcie_pb2.CreateNVMeSubsystemRequest(
-                nv_me_subsystem=frontend_nvme_pcie_pb2.NVMeSubsystem(
-                    spec=frontend_nvme_pcie_pb2.NVMeSubsystemSpec(
-                        id=object_key_pb2.ObjectKey(value="opi-subsystem"),
-                        model_number="OPI Model",
-                        serial_number="OPI SN",
-                        max_namespaces=10,
-                        nqn="nqn.2022-09.io.spdk:opi1",
+        with grpc.insecure_channel(address) as channel:
+            stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
+            res = stub.CreateNVMeSubsystem(
+                request=frontend_nvme_pcie_pb2.CreateNVMeSubsystemRequest(
+                    nv_me_subsystem=frontend_nvme_pcie_pb2.NVMeSubsystem(
+                        spec=frontend_nvme_pcie_pb2.NVMeSubsystemSpec(
+                            id=object_key_pb2.ObjectKey(value="opi-subsystem"),
+                            model_number="OPI Model",
+                            serial_number="OPI SN",
+                            max_namespaces=10,
+                            nqn="nqn.2022-09.io.spdk:opi1",
+                        )
                     )
                 )
             )
-        )
-        print(res)
+            print(res)
     except grpc.RpcError as e:
         print(e)
 
 
 def create_nvme_controller(address):
-    channel = grpc.insecure_channel(address)
-    stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
     try:
-        res = stub.CreateNVMeController(
-            request=frontend_nvme_pcie_pb2.CreateNVMeControllerRequest(
-                nv_me_controller=frontend_nvme_pcie_pb2.NVMeController(
-                    spec=frontend_nvme_pcie_pb2.NVMeControllerSpec(
-                        id=object_key_pb2.ObjectKey(value="opi-ctrl"),
-                        subsystem_id=object_key_pb2.ObjectKey(value="opi-subsystem"),
-                        pcie_id=opicommon_pb2.PciEndpoint(
-                            physical_function=1, virtual_function=2, port_id=3
-                        ),
-                        max_nsq=5,
-                        max_ncq=6,
-                        sqes=7,
-                        cqes=8,
-                        nvme_controller_id=1,
+        with grpc.insecure_channel(address) as channel:
+            stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
+            res = stub.CreateNVMeController(
+                request=frontend_nvme_pcie_pb2.CreateNVMeControllerRequest(
+                    nv_me_controller=frontend_nvme_pcie_pb2.NVMeController(
+                        spec=frontend_nvme_pcie_pb2.NVMeControllerSpec(
+                            id=object_key_pb2.ObjectKey(value="opi-ctrl"),
+                            subsystem_id=object_key_pb2.ObjectKey(value="opi-subsystem"),
+                            pcie_id=opicommon_pb2.PciEndpoint(
+                                physical_function=1, virtual_function=2, port_id=3
+                            ),
+                            max_nsq=5,
+                            max_ncq=6,
+                            sqes=7,
+                            cqes=8,
+                            nvme_controller_id=1,
+                        )
                     )
                 )
             )
-        )
-        print(res)
+            print(res)
     except grpc.RpcError as e:
         print(e)
 
 
 def create_nvme_namespace(address):
-    channel = grpc.insecure_channel(address)
-    stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
     try:
-        res = stub.CreateNVMeNamespace(
-            request=frontend_nvme_pcie_pb2.CreateNVMeNamespaceRequest(
-                nv_me_namespace=frontend_nvme_pcie_pb2.NVMeNamespace(
-                    spec=frontend_nvme_pcie_pb2.NVMeNamespaceSpec(
-                        id=object_key_pb2.ObjectKey(value="opi-ns"),
-                        subsystem_id=object_key_pb2.ObjectKey(value="opi-subsystem"),
-                        volume_id=object_key_pb2.ObjectKey(value="Malloc1"),
-                        uuid=uuid_pb2.Uuid(
-                            value="1b4e28ba-2fa1-11d2-883f-b9a761bde3fb"
-                        ),
-                        nguid="1b4e28ba-2fa1-11d2-883f-b9a761bde3fb",
-                        eui64=1967554867335598546,
-                        host_nsid=1,
+        with grpc.insecure_channel(address) as channel:
+            stub = frontend_nvme_pcie_pb2_grpc.FrontendNvmeServiceStub(channel)
+            res = stub.CreateNVMeNamespace(
+                request=frontend_nvme_pcie_pb2.CreateNVMeNamespaceRequest(
+                    nv_me_namespace=frontend_nvme_pcie_pb2.NVMeNamespace(
+                        spec=frontend_nvme_pcie_pb2.NVMeNamespaceSpec(
+                            id=object_key_pb2.ObjectKey(value="opi-ns"),
+                            subsystem_id=object_key_pb2.ObjectKey(value="opi-subsystem"),
+                            volume_id=object_key_pb2.ObjectKey(value="Malloc1"),
+                            uuid=uuid_pb2.Uuid(
+                                value="1b4e28ba-2fa1-11d2-883f-b9a761bde3fb"
+                            ),
+                            nguid="1b4e28ba-2fa1-11d2-883f-b9a761bde3fb",
+                            eui64=1967554867335598546,
+                            host_nsid=1,
+                        )
                     )
                 )
             )
-        )
-        print(res)
+            print(res)
     except grpc.RpcError as e:
         print(e)
